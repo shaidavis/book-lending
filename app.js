6acb0e16283ca0b6fda87445808e5185e9a09615
Shai Davis <shaidavis@MBP.local>
2016-08-16 11:05:04 +0300

    added book-display

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/books');
 
-var Book = require("./models/BookModel");
+// var Book = require("./models/BookModel");
 
 var app = express();
 console.log("STATUS: you're running app.js. don't forget to restart the server if you make changes")
@@ -15,3 +15,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 app.use(express.static('templates'));
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + "/index.html");
+})
+
+
+
+app.listen(8000);
